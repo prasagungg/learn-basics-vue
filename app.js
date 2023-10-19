e0f2f3bfee01c9e6802a34eed0abbcece1a910df
prasagungg <agungprasetio878@gmail.com>
2023-10-19 07:51:19 +0700

    v-for

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const app = Vue.createApp({
       age: 45,
       x: 0,
       y: 0,
+      books: [
+        { title: "book - 1", author: "agung" },
+        { title: "book - 2", author: "agung" },
+        { title: "book - 3", author: "agung" },
+      ],
     };
   },
   methods: {
