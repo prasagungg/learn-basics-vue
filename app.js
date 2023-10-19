e7a7a0ed92c5d048074618ec6e0ba81d6e367b48
prasagungg <agungprasetio878@gmail.com>
2023-10-19 07:46:27 +0700

    mouse event

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const app = Vue.createApp({
       title: "The Final Empire",
       autor: "agung",
       age: 45,
+      x: 0,
+      y: 0,
     };
   },
   methods: {
@@ -13,6 +15,17 @@ const app = Vue.createApp({
     },
     toggleShowBooks() {
       this.showBooks = !this.showBooks;
+    },
+    handleEvent(event, data) {
+      console.log(event);
+
+      if (data) {
+        console.log(data);
+      }
+    },
+    handleMousemove(e) {
+      this.x = e.offsetX;
+      this.y = e.offsetY;
     },
   },
 });
