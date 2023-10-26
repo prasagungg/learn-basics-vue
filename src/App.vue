06cbc81f6ff4630d49a3798bde645fdc599f196f
prasagungg <agungprasetio878@gmail.com>
2023-10-26 23:32:14 +0700

    sign up form

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,29 @@
 <template>
   <h1>hello world</h1>
+  <SignupForm />
 </template>
 
 <script>
+import SignupForm from "./components/SignupForm.vue";
+
 export default {
+  components: { SignupForm },
   name: "App",
 };
 </script>
 
-<style></style>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2e3e50;
+  margin-top: 60px;
+}
+
+body {
+  margin: 0;
+  background: #eeeeee;
+}
+</style>
