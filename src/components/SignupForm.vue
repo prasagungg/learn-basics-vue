07c12ec80c12aa60a6f43c900c604068dea1b5b0
prasagungg <agungprasetio878@gmail.com>
2023-10-27 14:49:27 +0700

    click event skill

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -16,6 +16,12 @@
       <input type="checkbox" v-model="terms" required />
       <label>Accept terms and conditions</label>
     </div>
+
+    <label>skills: </label>
+    <input type="text" v-model="tempSkill" @keyup.alt="addSkill" />
+    <div v-for="skill in skills" :key="skill" class="pill">
+      <span @click="deleteSkill(skill)">{{ skill }}</span>
+    </div>
   </form>
 
   <p>Email : {{ email }}</p>
@@ -32,7 +38,22 @@ export default {
       password: "",
       role: "",
       terms: true,
+      tempSkill: "",
+      skills: [],
     };
+  },
+  methods: {
+    addSkill(e) {
+      if (e.key === "," && this.tempSkill) {
+        if (!this.skills.includes(this.tempSkill)) {
+          this.skills.push(this.tempSkill);
+        }
+        this.tempSkill = "";
+      }
+    },
+    deleteSkill(skill) {
+      this.skills = this.skills.filter((item) => skill !== item);
+    },
   },
 };
 </script>
@@ -72,5 +93,18 @@ input[type="checkbox"] {
   margin: 0 10px 0 0;
   position: relative;
   top: 2px;
+}
+
+.pill {
+  display: inline-block;
+  margin: 20px 10px 0 0;
+  padding: 6px 12px;
+  background: #eee;
+  border-radius: 20px;
+  font-size: 12px;
+  letter-spacing: 1px;
+  font-weight: bold;
+  color: #777;
+  cursor: pointer;
 }
 </style>
