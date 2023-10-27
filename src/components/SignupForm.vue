0b11030025ad424042f96c2f33d67240c0e1a568
prasagungg <agungprasetio878@gmail.com>
2023-10-28 01:57:19 +0700

    fix: validate

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,10 +1,11 @@
 <template>
-  <form>
+  <form @submit.prevent="handleSubmit">
     <label>Email</label>
     <input type="email" required v-model="email" />
 
     <label>Password</label>
-    <input type="email" required v-model="password" />
+    <input type="password" required v-model="password" />
+    <div v-if="passwordError" class="error">{{ passwordError }}</div>
 
     <label>Role: </label>
     <select v-model="role">
@@ -21,6 +22,10 @@
     <input type="text" v-model="tempSkill" @keyup.alt="addSkill" />
     <div v-for="skill in skills" :key="skill" class="pill">
       <span @click="deleteSkill(skill)">{{ skill }}</span>
+    </div>
+
+    <div class="submit">
+      <button>Create an Account</button>
     </div>
   </form>
 
@@ -40,6 +45,7 @@ export default {
       terms: true,
       tempSkill: "",
       skills: [],
+      passwordError: "",
     };
   },
   methods: {
@@ -53,6 +59,18 @@ export default {
     },
     deleteSkill(skill) {
       this.skills = this.skills.filter((item) => skill !== item);
+    },
+    handleSubmit() {
+      this.passwordError =
+        this.password.length > 5 ? "" : "Password harus diatas lima";
+
+      if (!this.passwordError) {
+        console.log("email :", this.email);
+        console.log("password :", this.password);
+        console.log("role :", this.role);
+        console.log("skills :", this.skills);
+        console.log("terms accepted :", this.terms);
+      }
     },
   },
 };
@@ -106,5 +124,24 @@ input[type="checkbox"] {
   font-weight: bold;
   color: #777;
   cursor: pointer;
+}
+button {
+  background: #0b6dff;
+  border: 0;
+  padding: 10px 20px;
+  margin-top: 20px;
+  color: white;
+  border-radius: 20px;
+}
+
+.submit {
+  text-align: center;
+}
+
+.error {
+  color: #ff0062;
+  margin-top: 10px;
+  font-size: 0.8em;
+  font-weight: bold;
 }
 </style>
